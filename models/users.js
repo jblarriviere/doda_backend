5746ef87f5b2dc3049cc7f4f315401c683d9c05f
Your Name <you@example.com>
2021-10-25 10:19:11 +0200

    add userModel et signUp route

---
+++ models/users.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var uid2 = require('uid2');
+var bcrypt = require('bcrypt')
+
+var usersSchema = mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    token: String,
+    birthday: Date,
+    nationality: String,
+    interests: [String],
+    trips: [TripSchema],
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'activities' }],
+    dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'activities' }]
+
+});
+
+var usersModel = mongoose.model('users', usersSchema);
+
+module.exports = usersModel;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var uid2 = require('uid2');
+var bcrypt = require('bcrypt')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//ROUTE SIGN-UP
+
+router.post('/sign-up', async function(req, res, next) {
+
+  const hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
+
+  var result = false
+
+  var newUser = new usersModel({
+    username: req.body.usernameFromFront,
+    email: req.body.emailFromFront,
+    password: hash,
+    token: uid2(32),
+    birthday: req.body.birthdayFromFront,
+    nationality: req.body.nationalityFromFront
+  })
+
+  var userSave = await newUser.save()
+
+  if(userSave) {
+    result = true
+  }
+  
+  res.json({result, token: userSave.token})
+
+});
+
 
 module.exports = router;
