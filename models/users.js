ec428dc540b26b73de5925df0358c939e43e4580
Your Name <you@example.com>
2021-10-25 18:35:52 +0200

    resolution bugs dans les routes sign in et sign up

--- models/users.js
+++ models/users.js
@@ -2,18 +2,23 @@ var mongoose = require('mongoose');
 var uid2 = require('uid2');
 var bcrypt = require('bcrypt')
 
+var TripSchema = mongoose.Schema({
+    budget: Number,
+    date: Date,
+    activities: [{type: mongoose.Schema.Types.ObjectId, ref: 'activities'}]
+})
+
 var usersSchema = mongoose.Schema({
     username: String,
     email: String,
     password: String,
     token: String,
-    birthday: String,
+    birthday: Date,
     nationality: String,
-    //interests: [String],
-    //trips: [TripSchema],
-    //likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'activities' }],
-    //dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'activities' }]
-
+    interests: [String],
+    trips: [TripSchema],
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'activities' }],
+    dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'activities' }]
 });
 
 var usersModel = mongoose.model('users', usersSchema);
--- routes/index.js
+++ routes/index.js
@@ -6,54 +6,69 @@ var bcrypt = require('bcrypt')
 
 var usersModel = require('../models/users')
 
+const parseDate = strDate => {
+  let newStr = strDate.split('/')
+    .reverse()
+    .join('-');
+  let date = new Date(newStr);
+  return date;
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 //ROUTE SIGN-UP
 
-router.post('/sign-up', async function(req, res, next) {
+router.post('/sign-up', async function (req, res, next) {
 
   const hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
 
   var result = false
 
-  var newUser = new usersModel({
-    username: req.body.usernameFromFront,
-    email: req.body.emailFromFront,
-    password: hash,
-    token: uid2(32),
-    birthday: req.body.birthdayFromFront,
-    nationality: req.body.nationalityFromFront
-  })
+  var findUser = await usersModel.findOne({ email: req.body.emailFromFront })
 
-  var userSave = await newUser.save()
+  if (!findUser) {
 
-  if(userSave) {
-    result = true
+    var newUser = new usersModel({
+      username: req.body.usernameFromFront,
+      email: req.body.emailFromFront,
+      password: hash,
+      token: uid2(32),
+      birthday: parseDate(req.body.birthdayFromFront),
+      nationality: req.body.nationalityFromFront
+    })
+
+    var userSave = await newUser.save()
+
+    if (userSave) {
+      result = true
+      res.json({ result, token: userSave.token })
+    }
   }
-  
-  res.json({result, token: userSave.token})
 
+
+  res.json({ result })
 });
 
 //ROUTE SIGN-IN
 
-router.post('/sign-in', async function(req, res, next) {
-  
-  let result = false;
+router.post('/sign-in', async function (req, res, next) {
 
-  let findUser = await usersModel.findOne({email:req.body.emailFromFront})
+  let findUser = await usersModel.findOne({ email: req.body.emailFromFront })
 
-  let password = req.body.passwordFromFront;
+  if (findUser) {
+    let password = req.body.passwordFromFront;
 
-  if (bcrypt.compareSync(password, findUser.password)) {
-    res.json({ login: true, token: findUser.token });
-   } else {
-    res.json({ login: false });
-   }
+    if (bcrypt.compareSync(password, findUser.password)) {
+      res.json({ login: true, token: findUser.token });
+    } else {
+      res.json({ login: false });
+    }
+  } 
+
+      res.json({login: false})
 });
 
 
